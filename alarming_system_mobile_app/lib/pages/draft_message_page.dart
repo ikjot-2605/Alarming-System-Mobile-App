4b667beeebf37bc9ed6e0431ec951a2fa94625fb
Ikjot Singh <ikjotsd.191cs123@nitk.edu.in>
2020-10-06 02:50:49 +0530

    message draft page ui improved

--- alarming_system_mobile_app/lib/pages/draft_message_page.dart
+++ alarming_system_mobile_app/lib/pages/draft_message_page.dart
@@ -2,13 +2,13 @@ import 'package:alarming_system_mobile_app/model/AppUser.dart';
 import 'package:alarming_system_mobile_app/pages/home_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flushbar/flushbar.dart';
+import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
 class DraftMessagePage extends StatefulWidget {
   final AppUser appUser;
-  DraftMessagePage(this.appUser, {Key key})
-      : super(key: key);
+  DraftMessagePage(this.appUser, {Key key}) : super(key: key);
   @override
   _DraftMessagePageState createState() => _DraftMessagePageState();
 }
@@ -27,106 +27,124 @@ class _DraftMessagePageState extends State<DraftMessagePage> {
             : Colors.grey[900],
       ),
       body: SingleChildScrollView(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+            Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 10),
+            child: Text(
+              'Your Current Message',
+              style: TextStyle(fontWeight: FontWeight.w600),
+            ),
+          ),
+          Center(
+            child: Container(
+                width: MediaQuery.of(context).size.width / 1.1,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Colors.grey[300],
+                ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text('Your Current Message'),
-                ),
-              ),
-              FutureBuilder<String>(
-                future: getMessageFromHive(),
-                builder:
-                    (BuildContext context, AsyncSnapshot<String> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (snapshot.hasData) {
-                    return Center(
-                      child: Text(snapshot.data),
-                    );
-                  } else if (snapshot.data == null) {
-                    return Center(
-                      child: Text("You haven't set up a message yet"),
-                    );
-                  } else
-                    return Scaffold(
-                      appBar: AppBar(
-                        title: Text('Error Screen'),
-                      ),
-                    );
-                  return Container();
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('New Message'),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 3,
-                  child: TextFormField(
-                    controller: _messageController,
-                    textInputAction: TextInputAction.send,
-                    keyboardType: TextInputType.text,
-                    maxLines: 5,
-                    decoration: InputDecoration(
-                      fillColor: Colors.black,
-                      hintText: 'Emergency Message',
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                          5,
-                        ),
-                        borderSide: BorderSide(
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                          5,
-                        ),
-                        borderSide: BorderSide(
-                          color: Colors.grey[400],
-                          width: 2,
-                        ),
-                      ),
+                  child: Center(child: Text(widget.appUser.emergencyMessage)),
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 10),
+            child: Text(
+              'New Message',
+              style: TextStyle(fontWeight: FontWeight.w600),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Container(
+              child: TextFormField(
+                controller: _messageController,
+                textInputAction: TextInputAction.send,
+                keyboardType: TextInputType.text,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  fillColor: Colors.black,
+                  hintText: 'Emergency Message',
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      5,
                     ),
-                    textCapitalization: TextCapitalization.sentences,
-                    // ignore: missing_return
+                    borderSide: BorderSide(
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      5,
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.grey[400],
+                      width: 2,
+                    ),
                   ),
                 ),
+                textCapitalization: TextCapitalization.sentences,
+                // ignore: missing_return
               ),
-              Center(
-                child: Container(
-                  height: 40,
-                  width: MediaQuery.of(context).size.width / 1.15,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(5.0))),
-                    textColor: Colors.white,
-                    child: Text("Save Message"),
-                    onPressed: () {
-                      if (_messageController.text == "" ||
-                          _messageController.text == null) {
-                        Flushbar(
-                          title: "Error",
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.announcement,
+                    size: 12,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Please keep the message to a\nmaximum of 100 characters',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: Center(
+              child: Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width / 1.15,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                  textColor: Colors.white,
+                  child: Text("Save Message"),
+                  onPressed: () {
+                    if (_messageController.text == "" ||
+                        _messageController.text == null) {
+                      FlushbarHelper.createError(
                           message:
-                              "Please ensure you enter a valid message containing more than zero characters",
-                          duration: Duration(seconds: 3),
-                        )..show(context);
-                      } else {
-                        storeDetailsInHive(_messageController.text);
-                      }
-                    },
-                    color: Color(0xFF6770D2),
-                  ),
+                              "Please ensure you enter a valid message with more than zero characters",
+                          duration: Duration(seconds: 2))
+                        ..show(context);
+                    } else if (_messageController.text.length > 100) {
+                      FlushbarHelper.createError(
+                          message:
+                              "Please ensure you enter a message with less than 100 characters",
+                          duration: Duration(seconds: 2))
+                        ..show(context);
+                    } else {
+                      storeDetailsInHive(_messageController.text);
+                    }
+                  },
+                  color: Color(0xFF6770D2),
                 ),
               ),
-            ]),
+            ),
+          ),
+        ]),
       ),
     );
   }
@@ -137,9 +155,9 @@ class _DraftMessagePageState extends State<DraftMessagePage> {
 
   void storeDetailsInHive(String message) async {
     CollectionReference users = FirebaseFirestore.instance.collection('users');
-    return users.doc(widget.appUser.firebaseId).update({
-      'message':message
-    }).then((value){
+    return users
+        .doc(widget.appUser.firebaseId)
+        .update({'message': message}).then((value) {
       AppUser toSend = new AppUser(
         name: widget.appUser.name,
         email: widget.appUser.email,
@@ -150,7 +168,8 @@ class _DraftMessagePageState extends State<DraftMessagePage> {
         emergencyContacts: widget.appUser.emergencyContacts,
         emergencyMessage: message,
       );
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage(toSend)));
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage(toSend)));
       Flushbar(
         title: "Success",
         message: "Your message was saved successfully",
